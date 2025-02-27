a4deed60ccb4619e772f82956cfc004d0a3b589d
kiranangadi2008 <kiranangadi51@gmail.com>
2025-02-27 23:05:18 +0530

    new animations

--- script.js
+++ script.js
@@ -61,34 +61,187 @@ boximg2.addEventListener("mouseleave", function(){
 })
 
 
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 1,
-    spaceBetween: 30,
-    loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
-
-
-let swip1 = document.querySelector(".swiper-wrapper")
-
-swip1.addEventListener("mouseenter", function(){
-    cursor.innerHTML = "Drag",
-    cursor.style.fontSize = "2vw"
-    gsap.to(cursor, {
-        scale:2
+function swiperanim(){
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 30,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+    
+    
+    let swip1 = document.querySelector(".swiper-wrapper")
+    
+    swip1.addEventListener("mouseenter", function(){
+        cursor.innerHTML = "Drag",
+        cursor.style.fontSize = "2vw"
+        gsap.to(cursor, {
+            scale:2
+        })
     })
+    swip1.addEventListener("mouseleave", function(){
+        cursor.innerHTML = "",
+        cursor.style.fontSize = ""
+        gsap.to(cursor, {
+            scale:1
+        })
+    })
+}
+
+swiperanim()
+
+let tl1 = gsap.timeline()
+
+
+tl1.from("h2 .first", {
+    y:50,
+    duration:1,
+    stagger:0.15,
+    opacity:0,
+    delay:1
 })
-swip1.addEventListener("mouseleave", function(){
-    cursor.innerHTML = "",
-    cursor.style.fontSize = ""
-    gsap.to(cursor, {
-        scale:1
-    })
+
+
+tl1.to(".black",{
+    opacity:0,
+    // delay:1,
+    duration:3
+})
+
+tl1.from("h1 .first", {
+    y:200,
+    duration:1,
+    stagger:0.15,
+    opacity:0,
+    // delay:1
+})
+
+gsap.from(".textarea2 h1", {
+    y:50,
+    duration:1,
+    stagger:0.15,
+    opacity:0,
+    delay:1,
+    scrollTrigger: {
+        trigger: ".textarea2 h1",
+        scroller: "body",
+        start: "0% 50%",
+        end: "0% 55%",
+        scrub: 3,
+        // markers:true
+    }
+})
+
+let tl2 = gsap.timeline()
+
+
+tl2.from(".textarea1 h2", {
+    y:50,
+    duration:1,
+    stagger:0.15,
+    opacity:0,
+    delay:1,scrollTrigger: {
+        trigger: ".textarea1 h2",
+        scroller: "body",
+        start: "0% 50%",
+        end: "0 55%",
+        scrub: 3,
+        // markers:true
+    }
+})
+tl2.from(".textarea3 h2", {
+    y:50,
+    duration:0.5,
+    stagger:0.15,
+    opacity:0,
+    scrollTrigger: {
+        trigger: ".textarea2 h2",
+        scroller: "body",
+        start: "0% 50%",
+        end: "0 55%",
+        scrub: 3,
+        // markers:true
+    }
+})
+tl2.from(".textarea4 h2", {
+    y:50,
+    duration:0.5,
+    stagger:0.15,
+    opacity:0,
+    scrollTrigger: {
+        trigger: ".textarea2 h2",
+        scroller: "body",
+        start: "0% 50%",
+        end: "0 55%",
+        scrub: 3,
+        // markers:true
+    }
+})
+tl2.from(".textarea5 h2", {
+    y:50,
+    duration:0.5,
+    stagger:0.15,
+    opacity:0,
+    scrollTrigger: {
+        trigger: ".textarea2 h2",
+        scroller: "body",
+        start: "0% 50%",
+        end: "0 55%",
+        scrub: 3,
+        // markers:true
+    }
+})
+tl2.from(".textarea6 h2", {
+    y:50,
+    duration:0.5,
+    stagger:0.15,
+    opacity:0,
+    scrollTrigger: {
+        trigger: ".textarea2 h2",
+        scroller: "body",
+        start: "0% 50%",
+        end: "0% 55%",
+        scrub: 3,
+        // markers:true
+    }
+})
+
+tl3 = gsap.timeline()
+
+tl3.from(".textarea10 h2", {
+    y:50,
+    duration:0.5,
+    stagger:0.3,
+    opacity:0,
+    scrollTrigger: {
+        trigger: ".textarea10 h2",
+        scroller: "body",
+        start: "0% 50%",
+        end: "0 55%",
+        scrub: 3,
+        // markers:true
+    }
+})
+
+gsap.to(".vid8 video", {
+    // y:70,
+    duration:1,
+    // opacity:0,
+    // height:"0",
+    width:"90vw",
+    scrollTrigger: {
+        trigger: ".vid8 video",
+        scroller: "body",
+        start: "30% 40%",
+        end: "30% 20%",
+        scrub: 3,
+        // markers:true
+    }
 })
